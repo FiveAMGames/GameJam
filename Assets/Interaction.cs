21412e318fbb848b1af879c65a9e8eb456ee8154
Sasha <sashok.net@mail.ru>
2017-01-21 10:27:22 +0100

    first assets logic

--- Assets/Interaction.cs
+++ Assets/Interaction.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Interaction : MonoBehaviour {
 
+
+	public GameObject nextStep;
+	private bool onMouse = false;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,18 @@ public class Interaction : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+
+		if (Input.GetMouseButton && onMouse && nextStep!=null) {
+			nextStep.SendMessage ("Go");
+		}
+
+
+	}
+	public void OnMouseEnter(){
+		onMouse = true;
+	}
+	public void OnMouseExit(){
+		onMouse = false;
 	}
 }
